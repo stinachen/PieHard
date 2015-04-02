c28d9f7d8278d8255d4fd822812fc9ea9a60601f
Anna Do <doannado@gmail.com>
2015-04-02 10:21:33 -0400

    animal spawn

---
+++ PieHard/Assets/Scripts/Animal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : MonoBehaviour {
+
+	private Vector3 spawnPoint;
+	public Vector3 bottomPoint;
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoint = gameObject.transform.position;
+		speed = 3f;
+		bottomPoint = gameObject.transform.position;
+		bottomPoint.y = -.38f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3.Lerp (spawnPoint, bottomPoint, Time.deltaTime * speed);
+	}
+}
---
+++ PieHard/Assets/Scripts/AnimalSpawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalSpawn : MonoBehaviour {
+
+	private float usage;
+	private bool set;
+	private float delay; 
+
+	public GameObject animal;
+
+	// Use this for initialization
+	void Start () {
+		set = false;
+		delay = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!set) {
+			usage = Time.time + delay;
+			set = true;
+			//instantiate animal
+			Instantiate(animal);
+			animal.transform.position = transform.position;
+		}
+		if (Time.time > usage) {
+			set = false;
+		}
+	}
+}
