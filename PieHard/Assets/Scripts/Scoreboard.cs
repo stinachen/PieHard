e31ac9c46be876556a0a584582222728257a6ec6
Anna Do <doannado@gmail.com>
2015-04-06 23:15:20 -0400

    resized buttons and fixed scoring

--- PieHard/Assets/Scripts/Scoreboard.cs
+++ PieHard/Assets/Scripts/Scoreboard.cs
@@ -12,8 +12,11 @@ public class Scoreboard : MonoBehaviour {
 	private DontDestroy information;
 	// Use this for initialization
 	void Start () {
+		dontDestroy = GameObject.FindGameObjectWithTag ("dontdestroy");
+		score = GameObject.FindGameObjectWithTag("finalScore").GetComponent<GUIText>();
 		information = dontDestroy.GetComponent<DontDestroy>();
 		score.text = "Score: " + information.totalScore;
+		//print ("total score " + information.totalScore);
 	}
 
 	public void playAgain(){
@@ -22,5 +25,9 @@ public class Scoreboard : MonoBehaviour {
 
 	public void mainMenu(){
 		Application.LoadLevel(0);
+	}
+
+	void Update(){
+		score.text = "Score: " + information.totalScore;
 	}
 }
